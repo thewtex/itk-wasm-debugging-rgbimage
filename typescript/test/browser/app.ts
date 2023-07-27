6d4d1168536ab517668eb8859101c16b4a12f825
Matt McCormick <matt.mccormick@kitware.com>
2023-07-27 18:43:31 -0400

    Add the ability to upload image, run through pipeline in test demo app

--- typescript/test/browser/app.ts
+++ typescript/test/browser/app.ts
@@ -1,5 +1,8 @@
 import * as rgbimageBindings from '../../dist/bundles/rgbimage-bindings.js'
 rgbimageBindings.setPipelinesBaseUrl('/pipelines')
+rgbimageBindings.setPipelineWorkerUrl('/web-workers/pipeline.worker.js')
+
+import { readImageFile } from 'itk-wasm'
 
 const packageFunctions = []
 for (const [key, val] of Object.entries(rgbimageBindings)) {
@@ -16,3 +19,27 @@ pipelineFunctionsList.innerHTML = `
 `
 console.log(packageFunctions)
 console.log(rgbimageBindings)
+
+const inputElement = document.querySelector('#rgbimage-inputs input[name=input-file]')
+inputElement.addEventListener('change', async (event) => {
+    const files = event.target.files
+    console.log(files[0])
+
+    const { webWorker, image } = await readImageFile(null, files[0])
+    console.log('input', image)
+
+    function replacer (key, value) {
+      if (!!value && value.byteLength !== undefined) {
+        return String(value.slice(0, 6)) + '...'
+      }
+      return value
+    }
+    const inputMetaDataElement = document.querySelector('#rgbimage-inputs sl-textarea[name=input]')
+    inputMetaDataElement.value = JSON.stringify(image, replacer, 2)
+
+    const { OutputImage } = await rgbimageBindings.RGBImage(null, image)
+    console.log(OutputImage)
+    const outputMetaDataElement = document.querySelector('#rgbimage-inputs sl-textarea[name=output]')
+    outputMetaDataElement.value = JSON.stringify(OutputImage, replacer, 2)
+
+})
